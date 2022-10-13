1fb267adec2a8e1c8e5c97f3a97cb48bbcd5264a
potatoy1 <ekyoung01121@gmail.com>
2022-10-13 20:03:44 +0900

    1013 update

--- FAST_AJAX/myajax.py
+++ FAST_AJAX/myajax.py
@@ -4,6 +4,14 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.responses import JSONResponse
 from emp_dao import EmpDao
+from pydantic.main import BaseModel
+
+
+class Emp(BaseModel):
+    e_id:str = None
+    e_name:str = None
+    sex:str = None
+    addr:str = None
 
 app = FastAPI()
 
@@ -21,4 +29,28 @@ async def emp_selects():
     ed = EmpDao()
     emps = ed.selects()
     return JSONResponse(emps)
+
+@app.post("/emp_select")
+async def emp_select(emp:Emp):
+    ed = EmpDao()
+    emp = ed.select(emp.e_id)
+    return JSONResponse(emp)
+
+@app.post("/emp_insert")
+async def emp_insert(emp:Emp):
+    ed = EmpDao()
+    cnt = ed.insert(emp.e_id,emp.e_name,emp.sex,emp.addr)
+    return JSONResponse(cnt)
+
+@app.post("/emp_update")
+async def emp_update(emp:Emp):
+    ed = EmpDao()
+    cnt = ed.update(emp.e_id,emp.e_name,emp.sex,emp.addr)
+    return JSONResponse(cnt)
+
+@app.post("/emp_delete")
+async def emp_delete(emp:Emp):
+    ed = EmpDao()
+    cnt = ed.delete(emp.e_id)
+    return JSONResponse(cnt)
     
